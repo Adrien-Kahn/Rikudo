685dbb4dc27e302686f8a90b878b01f5397834bc
Adrien Kahn <adrien.kaet@gmail.com>
2021-01-27 14:44:57 +0100

    added backtracking

--- Graph.java
+++ Graph.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -24,6 +25,9 @@ public class Graph {
 	}
 	
 	
+	
+	
+	// Task 1
 	
 	// x_i,v (the i-th vertex in the path is v) is represented by the integer i + n*v + 1 because for some reason sat4j can't deal with 0
 	// Therefore, x_k = x_i,v where i = (k - 1) % n and v = (k - 1) // n
@@ -126,6 +130,149 @@ public class Graph {
 	
 	
 	
+	// Task 2
+	
+	
+	private void exploring(int s, int t, int[] explored, ArrayList<Integer> path, int n) { 	// auxiliary recursive method used to explore the graph in search of an hamiltonian path. s is the last vertex added to the path, t is the goal.
+		ArrayList<Integer> neigh = neighbors(s);
+		for (int v : neigh) {
+			
+			if (v == t) {														
+				if (path.size() == n-1) {explored[t] = 1; path.add(t); break;} // t is necessarily the last vertex explored
+			}
+			
+			else {if (explored[v] == 0) {																		
+				explored[v] = 1;
+				path.add(v);
+				exploring(v,t,explored,path,n);
+				if (path.size() == n) break; // determines whether the path + v can be completed into an hamiltonian path
+				explored[v] = 0; 
+				path.remove(path.size()-1);
+			}}
+			
+		}
+	}
+	
+	public ArrayList<Integer> hamiltonianBacktracking(int s, int t){
+		
+		// initialization
+		int n = vertexNumber();
+		int[] explored = new int[n+1];
+		explored[s] = 1;
+		ArrayList<Integer> path = new ArrayList<Integer>();
+		path.add(s);
+		
+		// exploring the graph with the auxiliary method
+		exploring(s,t, explored, path, n);
+		
+		//result
+		if (path.size() == n) {System.out.println("Congrats you have found a hamiltonian path"); System.out.println(path.toString());return path;}
+		System.out.println("No hamiltonian path here sir"); return new ArrayList<Integer>();
+	}
+	
+	// Remark 2 : variation 1, counting hamiltonian paths
+	
+	private void exploring_counting(int s, int t, int[] explored, ArrayList<Integer> path, int n, ArrayList<ArrayList<Integer>> mem) { 
+		ArrayList<Integer> neigh = neighbors(s);
+		for (int v : neigh) {
+			
+			if (v == t) {
+				if (path.size() == n-1) {
+				explored[t] = 1; 
+				path.add(t);
+				mem.add((ArrayList<Integer>)path.clone());
+				explored[t] = 0; 
+				path.remove(path.size()-1);
+			}}
+			
+			else {if (explored[v] == 0) {	
+				explored[v] = 1;
+				path.add(v);
+				exploring_counting(v,t,explored,path,n,mem);
+				explored[v] = 0; 
+				path.remove(path.size()-1);
+			}}
+			
+		}	
+	}
+	
+	public ArrayList<ArrayList<Integer>> hamiltonianBacktracking_counting(int s, int t){
+		
+		// initialization
+		int n = vertexNumber();
+		int[] explored = new int[n+1];
+		explored[s] = 1;
+		ArrayList<Integer> path = new ArrayList<Integer>();
+		path.add(s);
+		ArrayList<ArrayList<Integer>> mem = new ArrayList<ArrayList<Integer>>();
+		
+		// exploring the graph with the auxiliary method
+		exploring_counting(s,t, explored, path, n, mem );
+		
+		//result
+		System.out.println("Congrats you have found " + mem.size()  + " hamiltonian path"); 
+		System.out.println(mem);
+		return mem;
+	}
+	
+	
+	// Remark 2 : variation 2 : return a hamiltonian path if there are more than a certain number of them
+	
+	private void exploring_k(int s, int t, int k, int[] explored, ArrayList<Integer> path, int n, ArrayList<ArrayList<Integer>> mem) { 
+		ArrayList<Integer> neigh = neighbors(s);
+		for (int v : neigh) {
+			
+			if (v == t) {
+				if (path.size() == n-1) {
+				explored[t] = 1; 
+				path.add(t);
+				mem.add((ArrayList<Integer>)path.clone());
+				if (mem.size() == k) break;
+				explored[t] = 0; 
+				path.remove(path.size()-1);
+			}}
+			
+			else {if (explored[v] == 0) {	
+				explored[v] = 1;
+				path.add(v);
+				exploring_k(v,t,k, explored,path,n,mem);
+				explored[v] = 0; 
+				path.remove(path.size()-1);
+			}}
+			
+		}	
+	}
+	
+	public ArrayList<Integer> hamiltonianBacktracking_k(int s, int t,int k){
+		
+		// initialization
+		int n = vertexNumber();
+		int[] explored = new int[n+1];
+		explored[s] = 1;
+		ArrayList<Integer> path = new ArrayList<Integer>();
+		path.add(s);
+		ArrayList<ArrayList<Integer>> mem = new ArrayList<ArrayList<Integer>>();
+		// exploring the graph with the auxiliary method
+		
+		exploring_k(s,t,k,explored, path, n, mem );
+		
+		//result
+		if (mem.size() == k) {
+			System.out.println("There are more than " + k + " hamiltonian paths. Here is one example :");
+			System.out.println(mem.get(0));
+			return mem.get(0);
+		}
+		else {
+			System.out.println("There are less than " + k + " hamiltonian paths");
+			return new ArrayList<Integer>();
+		}
+	}
+
+	
+	
+	// Useful graphs
+	
+	
 	public static Graph completeGraph(int n) {
 		ArrayList<ArrayList<Integer>> al = new ArrayList<ArrayList<Integer>>();
 		for (int k = 0; k < n; k ++) {
@@ -148,6 +295,24 @@ public class Graph {
 		}
 		return new Graph(al);
 	}
+	
+	public static Graph gridGraph(int n) {
+		ArrayList<ArrayList<Integer>> al = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				int k = n*i +j;
+				ArrayList<Integer> a = new ArrayList<Integer>();
+				if (i > 0) a.add(k-n);
+				if (j > 0) a.add(k-1);
+				if (j < n-1) a.add(k+1);
+				if (i < n-1) a.add(k+n);
+				al.add(a);
+			}
+			
+		}
+		return new Graph(al);
+	}
+	
 	
 	
 	public static void main(String[] args) {
@@ -172,12 +337,26 @@ public class Graph {
 		Graph cg = completeGraph(100);
 		System.out.println(cg.adjacencyList);
 		cg.hamiltonianPath(5, 8);
-		*/
-		
-		
+		cg.hamiltonianBacktracking(7,6);
+
+
 		Graph cyg = cycleGraph(100);
 		System.out.println(cyg.adjacencyList);
 		cyg.hamiltonianPath(7, 6);
+		cyg.hamiltonianBacktracking(7, 6);
+		
+		
+		Graph gg1 = gridGraph(4);
+		System.out.println(gg1.adjacencyList);
+		gg1.hamiltonianBacktracking(0,15);
+		gg1.hamiltonianBacktracking_counting(1,15);
+		
+		Graph gg2 = gridGraph(3);
+		gg2.hamiltonianBacktracking_counting(1,15);
+
+		*/
+		
+		
 		
 	}
 
